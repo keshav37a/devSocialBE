dd671b00d0c6affc47f209525d9b77142d1e41c4
keshav37a <keshav37a@gmail.com>
2025-03-03 09:49:50 +0530

    add mongoose, connect to db, env variables, dotenv

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
+const dotenv = require('dotenv');
 const express = require('express');
-const PORT = 3000;
 
+dotenv.config();
+const { handleDBConnect } = require('./config/database');
+
+const EXPRESS_PORT = 7777;
 const app = express();
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-});
+
+handleDBConnect()
+    .then(() => {
+        app.listen(EXPRESS_PORT, () => {
+            console.log(`listening on port ${EXPRESS_PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(`error in connecting to db ${err}`);
+    });
 
 app.use('/test', (req, res) => {
     res.send('hello from the server');
